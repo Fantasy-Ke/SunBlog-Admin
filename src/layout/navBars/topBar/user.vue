ed0320855b365754a7405669d5e833eed0095ad4
Fantasy-Ke <85232349+Fantasy-Ke@users.noreply.github.com>
2023-10-31 08:43:07 +0800

    修改配置

--- src/layout/navBars/topBar/user.vue
+++ src/layout/navBars/topBar/user.vue
@@ -185,7 +185,7 @@ const onHandleCommandClick = (path: string) => {
 			})
 			.catch(() => {});
 	} else if (path === 'wareHouse') {
-		window.open('https://gitee.com/lyt-top/vue-next-admin');
+		window.open('https://github.com/Fantasy-Ke/SunBlog-Admin');
 	} else {
 		router.push(path);
 	}
