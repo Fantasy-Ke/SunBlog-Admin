97b3ab3f3a9dfd53e88b306a2bbe6ff612cb356c
Fantasy-Ke <2246080525@qq.com>
2023-11-19 22:21:47 +0800

    pinia 持久化

--- src/stores/userInfo.ts
+++ src/stores/userInfo.ts
@@ -1,29 +1,36 @@
 import { defineStore } from 'pinia';
 // import Cookies from 'js-cookie';
 // import { Session } from '@/utils/storage';
-import { computed, inject, reactive } from 'vue';
-import { UsersServiceProxy, ZUserInfoOutput } from '@/shared/service-proxies';
+import { computed, inject } from 'vue';
+import { UsersServiceProxy, ZFantasyToken } from '@/shared/service-proxies';
 import apiHttpClient from '@/utils/http';
+import { StoreKey, useUserStore } from './user';
 const _usersCService = new UsersServiceProxy(inject('$baseurl'), apiHttpClient as any);
 
 /**
  * 用户信息
  * @methods setUserInfos 设置用户信息
  */
-export const useUserInfo = defineStore('userInfo', () => {
-	const userInfoState = reactive({
-		userInfo: {} as ZUserInfoOutput,
-	});
+export const useUserInfo = defineStore(StoreKey.UserInfo, () => {
+	const userStore = useUserStore();
+	const userInfoState = computed(() => userStore);
 
 	//用户信息
-	const userInfo = computed(() => userInfoState.userInfo);
+	const userInfo = computed(() => userStore.userInfo);
 
+	const setToken = (zToken: ZFantasyToken) => {
+		userStore.setToken({ zToken: zToken });
+	};
 	/**
 	 * 获取当前用户基本信息
 	 */
 	const getUserInfo = async () => {
 		const { result } = await _usersCService.currentUserInfo();
-		userInfoState.userInfo = result!;
+		userStore.setUserInfo(result!);
 	};
-	return { userInfoState, userInfo, getUserInfo };
+	/**
+	 * 获取当前用户基本信息
+	 */
+
+	return { userInfoState, userInfo, getUserInfo, setToken };
 });
